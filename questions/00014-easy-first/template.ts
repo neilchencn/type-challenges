3323091af26b680e1124aace9d1a01f1fdcfa98a
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-04 16:54:26 +0800

    answer: NO.14 first of array

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,3 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [infer first, ...infer _others] ? first : never // infer
+// type First<T extends any[]> = T['length'] extends 0 ? never : T[0] // array length === 0
+// type First<T extends any[]> = T extends [] ? never : T[0] // array === []
