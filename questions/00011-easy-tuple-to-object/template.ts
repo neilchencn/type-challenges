9efd014c84816bc868e6cbafc4fb3ef6b8d5094c
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-04 16:19:15 +0800

    answer: NO.11 tuple to object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,6 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (string | number)[]> = {
+  [K in T[number] ]: K
+}
+
+// T[number] 用于遍历array
+// (string | number)[] 限定只能接受这两种类型, 应对@ts-expect-error
