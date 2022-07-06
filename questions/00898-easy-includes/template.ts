5e9e65498999810078f749d289905b8d4e4a5f94
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-06 15:38:09 +0800

    answer: NO.898 Includes & Equal

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,2 @@
-type Includes<T extends readonly any[], U> = any
+type Equal<T, U> = (<G>() => G extends T ? true : false) extends (<G>() => G extends U ? true : false) ? true : false
+type Includes<T extends readonly any[], U> = T extends [infer fst, ...infer rest] ? (Equal<fst, U> extends true ? true : Includes<rest, U>) : false
